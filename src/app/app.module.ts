e4e20aa800d2a3d3f159bd2158964a92a7fbf535
sasa-bonta <bonta2000@mail.ru>
2020-03-24 11:09:05 +0200

    Angular Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import { InfoComponent } from './info/info.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { MtbComponent } from './mtb/mtb.component';
+import { RoadComponent } from './road/road.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InfoComponent,
+    ProductsComponent,
+    ContactsComponent,
+    MtbComponent,
+    RoadComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        RouterModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
